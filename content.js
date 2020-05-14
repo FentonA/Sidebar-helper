02ee11a2a5330bc3dcdfc7c4b7708fb6c2c2e81b
Alf Fenton <Fentonalf@gmail.com>
2020-05-13 21:16:56 -0400

    reverted extension back to just checklists, slight enhanced css for list buttons

--- content.js
+++ content.js
@@ -12,51 +12,52 @@
 // 	window.location.href = url;
 // 	console.log("It's printing")
 // }
-window.onload = () =>{
-	const placeholders = document.createElement('button');
-	const houseads = document.createElement('button');
-	const cache = document.createElement('button');
+// window.onload = () =>{
+// 	const placeholders = document.createElement('button')
+// 	const houseads = document.createElement('button');
+// 	const cache = document.createElement('button');
 
-	placeholders.id = 'Placeholders';
-	houseads.id = 'houseads';
-	cache.id = 'cache';
+// 	placeholders.id = 'Placeholders';
+// 	houseads.id = 'houseads';
+// 	cache.id = 'cache';
 
-	placeholders.textContent = "Add Placeholders";
-	houseads.textContent = "Add houseads";
-	cache.textContent = "Add Cache";
+// 	placeholders.textContent = "Add Placeholders";
+// 	houseads.textContent = "Add houseads";
+// 	cache.textContent = "Add Cache";
 
-	document.querySelector("div:first-of-type").prepend(placeholders, houseads, cache);
-	placeholders.addEventListener('click', () => addPlaceholders());
-	houseads.addEventListener('click', ()=> addHouseAds());
-	cache.addEventListener('click', ()=>add123());
-}
+// 	document.querySelector("body").prepend(placeholders, houseads, cache);
+// 	placeholders.addEventListener('click', () => addPlaceholders());
+// 	houseads.addEventListener('click', ()=> addHouseAds());
+// 	cache.addEventListener('click', ()=>add123());
+// }
 
-function addPlaceholders(){
-	var url = window.location.href;
-	if (url.indexOf('?test') < 0){
-		url += '?test=placeholders';
-	}else{
-		console.log("there's one there already")
-	}
-	window.location.href = url;
-}
 
-function addHouseAds(){
-	var url = window.location.href;
-	if (url.indexOf('?test') < 0){
-		url += '?test=houseads';
-	}else{
-		console.log("there's one there already")
-	}
-	window.location.href = url;
-}
+// function addPlaceholders(){
+// 	var url = window.location.href;
+// 	if (url.indexOf('?test') < 0){
+// 		url += '?test=placeholders';
+// 	}else{
+// 		console.log("there's one there already")
+// 	}
+// 	window.location.href = url;
+// }
 
-function add123(){
-	var url = window.location.href;
-	if (url.indexOf('?test') < 0){
-		url += '?test=1234';
-	}else{
-		console.log("there's one there already")
-	}
-	window.location.href = url;
-}
+// function addHouseAds(){
+// 	var url = window.location.href;
+// 	if (url.indexOf('?test') < 0){
+// 		url += '?test=houseads';
+// 	}else{
+// 		console.log("there's one there already")
+// 	}
+// 	window.location.href = url;
+// }
+
+// function add123(){
+// 	var url = window.location.href;
+// 	if (url.indexOf('?test') < 0){
+// 		url += '?test=1234';
+// 	}else{
+// 		console.log("there's one there already")
+// 	}
+// 	window.location.href = url;
+// }
